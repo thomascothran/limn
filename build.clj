600672cddde771014683c427b9cd3dbd2c01ca2a
Thomas Cothran <tmcothran@gmail.com>
2025-01-12 17:18:03 -0500

    Build process

--- build.clj
+++ build.clj
@@ -1,27 +1,58 @@
 (ns build
   (:refer-clojure :exclude [test])
-  (:require [clojure.tools.build.api :as b] ; for b/git-count-revs
-            [org.corfield.build :as bb]))
+  (:require [clojure.tools.build.api :as b]
+            [deps-deploy.deps-deploy :as dd]))
 
-(def lib 'tech.thomascothran/limn)
+(def lib 'tech.thomascothran/pavlov)
+; alternatively, use MAJOR.MINOR.COMMITS:
 (def version (format "1.0.%s" (b/git-count-revs nil)))
+(def class-dir "target/classes")
 
-(defn test "Run the tests." [opts]
-  (bb/run-tests opts))
+(defn- pom-template [version]
+  [[:description "limn: a workflow engine for Clojure(Script)"]
+   [:url "https://github.com/thomascothran/limn"]
+   [:licenses
+    [:license
+     [:name "Eclipse Public License"]
+     [:url "http://www.eclipse.org/legal/epl-v10.html"]]]
+   [:developers
+    [:developer
+     [:name "Thomas Cothran"]]]
+   [:scm
+    [:url "https://github.com/thomascothran/limn"]
+    [:connection "scm:git:https://github.com/thomascothran/limn.git"]
+    [:developerConnection "scm:git:ssh:git@github.com:thomascothran/limn.git"]
+    [:tag (str "v" version)]]])
+
+(defn- jar-opts [opts]
+  (assoc opts
+         :lib lib   :version version
+         :jar-file  (format "target/%s-%s.jar" lib version)
+         :basis     (b/create-basis {})
+         :class-dir class-dir
+         :target    "target"
+         :src-dirs  ["src"]
+         :pom-data  (pom-template version)))
 
 (defn ci "Run the CI pipeline of tests (and build the JAR)." [opts]
-  (-> opts
-      (assoc :lib lib :version version)
-      (bb/run-tests)
-      (bb/clean)
-      (bb/jar)))
+  ;(test opts)
+  (b/delete {:path "target"})
+  (let [opts (jar-opts opts)]
+    (println "\nWriting pom.xml...")
+    (b/write-pom opts)
+    (println "\nCopying source...")
+    (b/copy-dir {:src-dirs ["resources" "src"] :target-dir class-dir})
+    (println "\nBuilding JAR..." (:jar-file opts))
+    (b/jar opts))
+  opts)
 
 (defn install "Install the JAR locally." [opts]
-  (-> opts
-      (assoc :lib lib :version version :description "Workflow engine for Clojure")
-      (bb/install)))
+  (let [opts (jar-opts opts)]
+    (b/install opts))
+  opts)
 
 (defn deploy "Deploy the JAR to Clojars." [opts]
-  (-> opts
-      (assoc :lib lib :version version)
-      (bb/deploy)))
+  (let [{:keys [jar-file] :as opts} (jar-opts opts)]
+    (dd/deploy {:installer :remote :artifact (b/resolve-path jar-file)
+                :pom-file (b/pom-path (select-keys opts [:lib :class-dir]))}))
+  opts)
